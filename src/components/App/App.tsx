134e4925b321b327d8745a8b9e9c54bf20356370
mafutka <mafutka@gmail.com>
2025-06-04 11:57:20 +0300

    styles-grid-problem

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,7 @@
 
 import { useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, keepPreviousData } from '@tanstack/react-query';
 import ReactPaginate from 'react-paginate';
-import toast, { Toaster } from 'react-hot-toast';
 import { fetchMovies } from '../../services/movieService';
 import type { Movie } from '../../types/movie';
 import SearchBar from '../SearchBar/SearchBar';
@@ -20,15 +19,13 @@ export default function App() {
   const [page, setPage] = useState(1);
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
   
-  const { data,  isLoading, isError, isFetching } = useQuery({
+  const { data,  isLoading, isError } = useQuery({
     queryKey: ['movies', query, page],
     queryFn: () => fetchMovies(query, page),
     enabled: !!query,
-    keepPreviousData: true,
+    placeholderData: keepPreviousData,
 
   })
-
-  console.log(data);
  
    const handleSearch = (searchQuery: string) => {
     setQuery(searchQuery);
@@ -45,13 +42,12 @@ export default function App() {
 
   return (
     <>
-      <Toaster />
       <SearchBar onSubmit={handleSearch} />
       {isLoading && <Loader />}
       {isError && <ErrorMessage />}
       {data && data.results.length > 0 && (
         <>
-         <MovieGrid movies={data.results} onSelect={handleSelectMovie} />
+         <MovieGrid movies={data.results.slice(0, 15)} onSelect={handleSelectMovie} />
           {data.total_pages > 1 && (
             <ReactPaginate
               pageCount={data.total_pages}
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -12,18 +12,18 @@ export interface ResponseMovie {
 const BASE_URL = 'https://api.themoviedb.org/3';
 const myToken = import.meta.env.VITE_TMDB_API_TOKEN;
 
-export const fetchMovies = async (query: string, page: number = 1): Promise<Movie[]> => {
+export const fetchMovies = async (query: string, page: number = 1): Promise<ResponseMovie> => {
   const response = await axios.get<ResponseMovie>(`${BASE_URL}/search/movie`, {
     params: { 
       query,
       language: 'en-US',
       include_adult: false,
       page,
-},
+    },
     headers: {
       Authorization: `Bearer ${myToken}`,
     },
   });
 
-  return response.data.results;
+  return response.data; 
 };
