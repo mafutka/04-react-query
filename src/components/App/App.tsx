f1ce54b9ee788c13414990e76e5675c5a306fc3f
mafutka <mafutka@gmail.com>
2025-06-04 11:08:13 +0300

    app-changed

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,16 @@
 
 import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import ReactPaginate from 'react-paginate';
 import toast, { Toaster } from 'react-hot-toast';
-import type { Movie } from '../../types/movie';
 import { fetchMovies } from '../../services/movieService';
+import type { Movie } from '../../types/movie';
 import SearchBar from '../SearchBar/SearchBar';
 import MovieGrid from '../MovieGrid/MovieGrid';
 import Loader from '../Loader/Loader';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import MovieModal from '../MovieModal/MovieModal';
-import { useQuery } from '@tanstack/react-query';
-import ReactPaginate from 'react-paginate';
+import css from './App.module.css'
 
  
 
@@ -17,12 +18,9 @@ export default function App() {
 
   const [query, setQuery] = useState('');
   const [page, setPage] = useState(1);
-   const [movies, setMovies] = useState<Movie[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
-
-  const { data, error: queryError, isLoading } = useQuery({
+  
+  const { data,  isLoading, isError, isFetching } = useQuery({
     queryKey: ['movies', query, page],
     queryFn: () => fetchMovies(query, page),
     enabled: !!query,
@@ -37,10 +35,6 @@ export default function App() {
     setPage(1);
   };
 
-  const handlePageChange = ({ selected }: { selected: number }) => {
-    setPage(selected + 1);
-  };
-
    const handleSelectMovie = (movie: Movie) => {
     setSelectedMovie(movie); 
   };
@@ -53,15 +47,27 @@ export default function App() {
     <>
       <Toaster />
       <SearchBar onSubmit={handleSearch} />
-      {isLoading && <p>...is loading</p>}
-      {loading && <Loader />}
-      {queryError && <ErrorMessage />}
-      {!loading && !error && (
-        <MovieGrid movies={movies} onSelect={handleSelectMovie} />
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+      {data && data.results.length > 0 && (
+        <>
+         <MovieGrid movies={data.results} onSelect={handleSelectMovie} />
+          {data.total_pages > 1 && (
+            <ReactPaginate
+              pageCount={data.total_pages}
+              pageRangeDisplayed={5}
+              marginPagesDisplayed={1}
+              onPageChange={({ selected }) => setPage(selected + 1)}
+              forcePage={page - 1}
+              containerClassName={css.pagination}
+              activeClassName={css.active}
+              nextLabel="→"
+              previousLabel="←"
+            />
+          )}
+        </>
       )}
-      {selectedMovie && (
-        <MovieModal movie={selectedMovie} onClose={handleCloseModal} />
-      )}
+      {selectedMovie && <MovieModal movie={selectedMovie} onClose={handleCloseModal} />}
     </>
   );
 }
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -2,22 +2,12 @@
 import axios from 'axios';
 import type { Movie } from '../types/movie';
 
-interface ResponseMovie {
+export interface ResponseMovie {
   page: number;
   results: Movie[];
   total_pages: number;
   total_results: number;
 }
-
-// Сервіс TMDB підтримує пагінацію, для цього вам потрібно передати у http-запиті 
-// додатково параметр page.
-
-// Тепер у відповідь бекенда вас буде цікавити не лише властивість results, 
-// а і total_page. Тому не забудьте оновити інтерфейс для типізації відповіді з бекенду.
-
-// Для відображення елементів пагінації використовуйте біліотеку React Paginate. 
-// Зверніть увагу, що вона містить готовий компонент пагінації, але без стилів. 
-// Тому додайте підготовлені нами стилі для неї у файл App.module.css:
 
 const BASE_URL = 'https://api.themoviedb.org/3';
 const myToken = import.meta.env.VITE_TMDB_API_TOKEN;
